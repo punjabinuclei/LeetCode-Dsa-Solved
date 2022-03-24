62ecf88f140528380df5a5ff13e1b347f3449635
Gurpreet Singh <62651343+guruXD0204@users.noreply.github.com>
2022-03-24 11:47:16 +0530

    Time: 728 ms (22.22%), Space: 89.9 MB (5.49%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
      
-        return totalWays(nums,0,target);
+        unordered_map<string,int>map;
+        
+        return totalWays(nums,0,target,map);
         
     }
     
     
     
-    int totalWays(vector<int>& nums, int currentIndex,int target)
+    int totalWays(vector<int>& nums, int currentIndex,int target,                unordered_map<string,int>&map)
     {
+        
+        
+        
+        
         
         if(currentIndex==nums.size() && target==0)
             return 1;
@@ -18,11 +24,20 @@ public:
             return 0;
         
         
-        int positive=totalWays(nums,currentIndex+1,target-nums[currentIndex]);
-        int negative=totalWays(nums,currentIndex+1,target+nums[currentIndex]);
+        
+        string currentKey=to_string(currentIndex)+"_"+to_string(target);
         
         
-        return positive+negative;
+        if(map.find(currentKey)!=map.end())
+            return map[currentKey];
+        
+        int positive=totalWays(nums,currentIndex+1,target-nums[currentIndex],map);
+        int negative=totalWays(nums,currentIndex+1,target+nums[currentIndex],map);
+        
+        
+        map[currentKey]=positive+negative;
+        
+        return map[currentKey];
         
         
     }
