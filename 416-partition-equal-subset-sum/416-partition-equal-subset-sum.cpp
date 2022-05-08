c8057f0fe70ab1b1cbc2de6660c166cbb8a8bfd1
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-05-08 09:21:33 +0530

    Time: 292 ms (40.97%), Space: 39.4 MB (29.76%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,55 +2,58 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         
-        
         unordered_map<string,bool>map;
         
         int totalSum=0;
-        for(int nums:nums)
-            totalSum+=nums;
+        
+        for(int sum:nums)
+            totalSum+=sum;
         
         if(totalSum%2!=0)
             return false;
         
-        return checkPartition(0,nums,totalSum/2,map);
+        return Partition(0,nums,totalSum/2,map);
             
     }
     
     
-    bool checkPartition(int currentIndex,vector<int>& nums,int targetSum,unordered_map<string,bool>&map)
+    bool Partition(int currentIndex,vector<int>&nums,int targetSum, unordered_map<string,bool> &map)
     {
-        if(currentIndex>=nums.size())
-            return false;
-        
         if(targetSum==0)
             return true;
         
+        if(currentIndex>=nums.size())
+            return false;
         
-        string currentKey=to_string(currentIndex)+ "_" +to_string(targetSum);
+         int currentPosValue=nums[currentIndex];
+        
+        string currentKey=to_string(currentIndex) + "-" + to_string(targetSum);
+        
         
         if(map.find(currentKey)!=map.end())
             return map[currentKey];
         
-        
         bool consider=false;
         
-        if(targetSum>=nums[currentIndex])
-        {
-            consider=checkPartition(currentIndex+1,nums,targetSum-nums[currentIndex],map);
-        }
-        if(consider){
-             map[currentKey] = consider;
-            return true;
-        }
-            
-        bool notConsider=checkPartition(currentIndex+1,nums,targetSum,map);
+        if(targetSum>=currentPosValue)
+            consider=Partition(currentIndex+1, nums, targetSum-currentPosValue,map);
+               if(consider)
+               {
+                   map[currentKey]=consider;
+        return map[currentKey];
+               }
         
+        bool notConsider=Partition(currentIndex+1, nums, targetSum,map);
         
-        map[currentKey]= (consider||notConsider);
+        map[currentKey]= consider || notConsider;
+        
         
         return map[currentKey];
-            
+        
     }
+    
+    
+    
     
     
     
