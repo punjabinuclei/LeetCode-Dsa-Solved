a4e965cc9636f389a2e65112e86cd61dc4295353
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-06-17 17:23:09 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (75.63%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,40 +1,56 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        vector<int> inorderTraversal(TreeNode *root)
-        {
-
-            vector<int> answer;
-
-            inorder(root, answer);
-
-            return answer;
-        }
-    
-    void inorder(TreeNode *root, vector<int>&answer)
-    {
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        stack<TreeNode*>stack;
+        
+        vector<int>ans;
         
         if(root==NULL)
-            return;
+            return ans;
         
+        pushLeftNode(root, stack);
         
-        
-        inorder(root->left,answer);
-        answer.push_back(root->val);
-        inorder(root->right,answer);
-        
-        return;
-        
+        while(!stack.empty())
+        {
+            TreeNode *currentNode=stack.top();
+            stack.pop();
+            ans.push_back(currentNode->val);
+            
+            
+            if(currentNode->right!=NULL)
+            {
+                pushLeftNode(currentNode->right,stack);
+            }
+            
+        }
+            
+       return ans; 
     }
+    
+    void pushLeftNode(TreeNode* root, stack<TreeNode*>&stack)
+    {
+        
+        stack.push(root);
+        
+        while(root->left!=NULL)
+        {
+            stack.push(root->left);
+            
+            root=root->left;
+        }
+}
+    
+    
 };
