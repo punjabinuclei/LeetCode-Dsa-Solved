d10ded74d1be5c6cfcf5aeb49dcbea6ef16b0413
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-06-22 15:41:18 +0530

    Time: 4 ms (42.70%), Space: 10.1 MB (44.45%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -15,11 +15,13 @@ class Solution
         bool isSameTree(TreeNode *p, TreeNode *q)
         {
 
-            
+           	//        we will store both tree nodes as a pair inside the queue
             queue<pair<TreeNode*, TreeNode*>> queue;
 
+           	//             we push both node values as a pair into the queue
             queue.push(make_pair(p, q));
 
+           	//           we perfrom the ususal bfs operation untill queue is empty
             while (!queue.empty())
             {
 
@@ -28,19 +30,20 @@ class Solution
                 TreeNode *pNode = values.first;
                 TreeNode *qNode = values.second;
 
+               	//                 If we find both nodes are null we move forward
                 if (pNode == NULL && qNode == NULL)
                     continue;
 
-                if (pNode == NULL || qNode == NULL || pNode->val!=qNode->val)
+                if (pNode == NULL || qNode == NULL || pNode->val != qNode->val)
                     return false;
-
+               	// recursive calls
                 if (pNode->left != NULL || qNode->left != NULL)
                     queue.push(make_pair(pNode->left, qNode->left));
 
                 if (pNode->right != NULL || qNode->right != NULL)
                     queue.push(make_pair(pNode->right, qNode->right));
             }
-            
+
             return true;
         }
 };
