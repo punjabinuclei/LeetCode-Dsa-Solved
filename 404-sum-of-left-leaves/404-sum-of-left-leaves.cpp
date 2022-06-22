3338bcb2ae05161c3b6b296e2447056ab4f3f6b5
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-06-22 16:15:22 +0530

    Time: 3 ms (85.63%), Space: 13.4 MB (44.15%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -1,41 +1,46 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int sumOfLeftLeaves(TreeNode *root)
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+//         We will
+        queue<pair<TreeNode*, bool>>q;
+        int totalSum=0;
+        
+        q.push(make_pair(root, false));
+        
+        
+        while(!q.empty())
         {
-
-            return LeftSum(root, false);
-        }
-
-    int LeftSum(TreeNode *root, bool onLeftLeaf)
-    {
-        if (root == NULL)
-            return 0;
-
-        if (root->left == NULL && root->right == NULL)
-        {
-            if (onLeftLeaf)
-                return root->val;
-        else
-            return 0;
+            pair<TreeNode*, bool> pairz=q.front();
+            q.pop();
+            
+            TreeNode* node=pairz.first;
+            bool isLeft=pairz.second;
+            
+            if(node->left==NULL && node->right==NULL && isLeft)
+            {
+                totalSum+=node->val;
+            }
+            
+            if(node->left!=NULL)
+                q.push(make_pair(node->left, true));
+            
+             if(node->right!=NULL)
+                q.push(make_pair(node->right, false));
+            
         }
         
-
-       int leftSum= LeftSum(root->left, true);
-        int rightSum=LeftSum(root->right, false);
-        
-        return leftSum+rightSum;
+        return totalSum;
         
     }
 };
