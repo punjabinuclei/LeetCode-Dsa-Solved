075b77d9383fdcfdc4eaa0e21a66e00458b2d1c5
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-06-28 13:27:38 +0530

    Time: 11 ms (68.66%), Space: 10.3 MB (20.28%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -5,6 +5,8 @@ public:
 //         we need a minHeap to get the smallest element on top and pop it.
         priority_queue<int, vector<int>, greater<int>>minHeap;
         
+//         traverse the array and keep on pushing into heap untill K if greater than k then we pop out thr top element which the the smallest element
+        
         for(int i=0;i<nums.size();i++)
         {
             minHeap.push(nums[i]);
