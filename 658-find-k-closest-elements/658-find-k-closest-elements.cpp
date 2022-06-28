63ef400a153ccef5d71a38d593843d9526abf05e
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-06-28 19:39:13 +0530

    Time: 202 ms (6.81%), Space: 34 MB (35.31%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+        vector<int> findClosestElements(vector<int> &arr, int k, int x)
+        {
+
+            priority_queue<pair<int, int>> maxHeap;
+            vector<int>answer;
+
+            for (int i = 0; i<arr.size(); i++)
+            {
+                maxHeap.push({ abs(arr[i] - x),
+                    arr[i] });
+
+                if (maxHeap.size() > k)
+                {
+                    maxHeap.pop();
+                }
+                
+            }
+            
+            while(!maxHeap.empty())
+            {
+                answer.push_back(maxHeap.top().second);
+                maxHeap.pop();
+            }
+            
+            sort(answer.begin(), answer.end());
+            
+            return answer; 
+        }
+};
