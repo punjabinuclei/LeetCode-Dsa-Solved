06e450c91dcca6924b986964b26644962a06d3f4
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-07-03 22:04:38 +0530

    Time: 18 ms (28.51%), Space: 11.5 MB (39.46%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
      
+//         MaxHeap as we need to sort in increasing frequency
+        
         priority_queue<pair<int,int>>maxHeap;
+//         we use map to store frequncy
         unordered_map<int,int>map;
         
         vector<int>answer;
@@ -12,7 +15,7 @@ public:
             map[nums[i]]++;
         }
         
-        
+//         we store negayive values Because we have used max heap. We want to print max frequency at the end. And if we put the negative frequency in a max heap, then it will behave like a min-heap, ie. if nums = [1,1,2,2,2,3] we make a map{nums[i], frequency} whose values will be-> ({1,2}, {2,3} {3,1}). Now, on pushing negative of frequency in max heap-> ({-1,3}, {-2,1}, {-3,2}). And while printing, we put abs(frequency) for printing an element 'frequency-number-of-times' and it will starting popping from top element which is 3 then 1 then 2.
         for(auto i=map.begin();i!=map.end();i++)
         {
             maxHeap.push({-i->second, i->first});
