f5b94a9266d84a3cdd23b2e84455cee4bb7b5d5b
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-07-06 19:08:08 +0530

    Time: 7 ms (14.30%), Space: 6 MB (25.46%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        // Iterative Approach 1
+//         if(n==0)
+//             return false;
+        
+//         while(n%2==0)
+//         {
+//             n=n/2;
+//         }
+        
+//         return n==1;
+        
+        
+//         ---------------------------------------------------------------
+
+        
+//         Recursive Approach 2
+        
         if(n==0)
             return false;
         
-        while(n%2==0)
-        {
-            n=n/2;
-        }
+        return (n==1|| n%2==0 && isPowerOfTwo(n/2));
         
-        return n==1;
     }
 };
