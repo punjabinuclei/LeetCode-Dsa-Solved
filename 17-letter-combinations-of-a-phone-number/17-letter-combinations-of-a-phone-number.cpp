83ed0c3ea7dfaaa3ac7668070320984dc8424c2b
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-07-13 21:16:45 +0530

    Time: 5 ms (19.74%), Space: 6.5 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         
+//         mappings of number to characters
         unordered_map<char,string>mp{
             {'1', ""}, 
             {'2', "abc"},
@@ -16,6 +17,7 @@ public:
         
          vector<string>ans;
         
+//         if string is empty then we return empty vector directly
         if(digits.size()==0) return ans;
         generateCombinations(digits,0,"",ans,mp);
         return ans;
@@ -23,10 +25,11 @@ public:
     }
     
     
-    
+//     Function to perform backtracking
     void generateCombinations(string digits, int currentIndex, string currentString, vector<string>&ans, unordered_map<char,string>&map)
     {
 
+//         if we reach the end we push the final string into answer and return from their
         if(currentIndex>=digits.size())
         {
             ans.push_back(currentString);
@@ -34,9 +37,13 @@ public:
         }
         
         
+//         we will transfer the first digit into a char variable
         char currentChar=digits[currentIndex];
+//         we will find the corresponding mapped string to the current digit in the map
         string mappings=map[currentChar];
         
+        // we will keep on updating our string 
+            
         for(int i=0;i<mappings.size();i++)
         {
             generateCombinations(digits,currentIndex+1,currentString+mappings[i],ans,map);
