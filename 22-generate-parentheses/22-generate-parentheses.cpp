8d16e90d27e822a40a742331ac68f844a5b49f7c
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-07-15 08:56:12 +0530

    Time: 6 ms (58.15%), Space: 13.7 MB (48.50%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -8,9 +8,12 @@ public:
         return ans;
     }
     
-    
+//     Function to store all combiantions in a vector of type string
     void combinations(int openCount, int closeCount, string expression, int n, vector<string>&ans)
     {
+//         if number of open brackets equals to close bracket and openBracket count is equal to
+//         n then we have found an expression and we store it into our vector of strings
+        
         if(openCount==n  && openCount==closeCount)
         {
             ans.push_back(expression);
