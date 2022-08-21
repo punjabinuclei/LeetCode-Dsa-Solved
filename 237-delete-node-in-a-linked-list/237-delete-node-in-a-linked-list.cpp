ee4a5c15b93e66139c9bb9eaa8c789fe38070e0f
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-08-21 11:53:18 +0530

    Time: 13 ms (87.70%), Space: 7.4 MB (92.52%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -10,6 +10,7 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
       
+//         CurrentNode value is made the next node value and then we point the currentnode next value to the next node next value
         ListNode *p;
         p=node->next;
         node->val=p->val;
