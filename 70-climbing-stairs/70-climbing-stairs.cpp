d74c97fc71c0cdc4852be23233bb2b3c7967c812
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-09-05 09:41:05 +0530

    Time: 5 ms (12.67%), Space: 6.5 MB (15.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,52 +1,29 @@
-class Solution
-{
-    public:
-        int climbStairs(int n)
-        {
-
-           	//          function to return the answer, we pass 
-           	//          the currentIndex and the target step as
-           	//          paramters in the function
+class Solution {
+public:
+    int climbStairs(int n) {
         
-//             we initialise a map to store overlapping subproblems
-//             to decrease the time complexity to O(n) from O(2^n)
-            
-            unordered_map<int,int>map;
-            return totalWays(0, n,map);
-        }
-
-   	// function to return totalWays to reach target    
-//     we pass map by reference
+        unordered_map<int,int>map;
+        
+     return totalWays(0,n, map);   
+    }
     
-    int totalWays(int currentIndex, int targetStep, unordered_map<int,int>&map)
+    int totalWays(int currentStep,int totalSteps, unordered_map<int,int>&map)
     {
-       	//baseCase
-        if (currentIndex > targetStep)
+        if(currentStep>totalSteps)
             return 0;
-
-        if (currentIndex == targetStep)
+        
+        if(currentStep==totalSteps)
             return 1;
-
-//         Map=key-value pair
-//         here the key will be the one thing thats changing ie
-//         the currentIndex will be the key and value will be what its returning
         
-        
-        int currentKey=currentIndex;
-        
-//         search for key in map, if found we directly return it or else we move forward
+        int currentKey=currentStep;
         
         if(map.find(currentKey)!=map.end())
             return map[currentKey];
         
-       	//Recursively call The functions for 1 steps and 2 steps
+        int oneStep=totalWays(currentStep+1, totalSteps, map);
+        int twoSteps=totalWays(currentStep+2, totalSteps, map);
         
-        int oneStep=totalWays(currentIndex+1, targetStep, map);
-        int twoStep=totalWays(currentIndex+2, targetStep, map);
-        
-//         we store the answer in the map directly for the currentIndex
-//         and return it
-        return  map[currentKey]=oneStep+twoStep;
-
+        return map[currentKey]=oneStep+twoSteps;
     }
+    
 };
