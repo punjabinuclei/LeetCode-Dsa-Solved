6f7400d1ac5be9049948ba94d11f7f6dfeb501ba
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-10-25 14:03:02 +0530

    Time: 16 ms (24.37%), Space: 10.7 MB (29.81%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,11 +1,15 @@
 class Solution {// Using Map & without sort
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>freq;
+        
+        unordered_map<int,int>freq;
+        
         vector<int>ans;
+        
         for(int i = 0;i<nums1.size();i++){
             freq[nums1[i]]++;
         }
+        
         for(int i = 0;i<nums2.size();i++){
             if (freq[nums2[i]] > 0){
                 freq[nums2[i]]--; 
