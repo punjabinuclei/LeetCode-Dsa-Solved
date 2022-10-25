ae9b049617f6fbae7adba253bcf159e5bae16864
Gurpreet Singh <62651343+punjabinuclei@users.noreply.github.com>
2022-10-25 13:24:19 +0530

    Time: 31 ms (33.22%), Space: 12.6 MB (32.98%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
-        int n = nums.size(); // gives the no. of rows
-        map<int,int> mp; // we don't need unordered_map because we need the elements to be in sorted format.
-        vector<int> vec;
+     
+        vector<int>answer;
+        map<int,int>map;
         
-        // traverse through the 2D array and store the frequency of each element
-        for(int row=0;row<n;row++)
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++)
         {
-            for(int col=0;col<nums[row].size();col++)
+            for(int j=0;j<nums[i].size();j++)
             {
-                mp[nums[row][col]]++;
+                map[nums[i][j]]++;
             }
         }
         
-        // In the 2D array, intersection occurs when the elements are present in every row.
-        // So the frequency of the element should match with the no. or rows in the 2D array.
-        for(auto element : mp)
-            if(element.second == n)
-                vec.push_back(element.first);
         
-        // return the intersecting elements
-        return vec;
+        for(auto element:map)
+        {
+            if(element.second==n)
+            {
+                answer.push_back(element.first);
+            }
+        }
+        
+        
+        return answer;
     }
 };
