f47fd75c7450449d2439b78111ead4151e4f61f3
Gurpreet Singh <guppi0204@gmail.com>
2026-06-14 17:51:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arraySum(List<Integer> arr) {
+        int sum=0;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            sum+=arr.get(i);
+        }
+        
+        return sum;
+    }
+};
