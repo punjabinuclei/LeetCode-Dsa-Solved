081c7ce73eb59d291ce5e926dadfe4ae6e80fa0a
Gurpreet Singh <guppi0204@gmail.com>
2026-09-15 21:54:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,28 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
-          
-          HashSet<Integer>set=new HashSet<>();
-        
-        for(int i=0;i<a.length;i++)
-        {
-            set.add(a[i]);
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        while (i < a.length && j < b.length) {
+            if (a[i] <= b[j]) {
+                if (!set.contains(a[i])) {
+                    ans.add(a[i]);
+                    set.add(a[i]);
+                }
+                if (a[i] == b[j]) {
+                    j++;
+                }
+                i++;
+            } else {
+                if (!set.contains(b[j])) {
+                    ans.add(b[j]);
+                    set.add(b[j]);
+                }
+                j++;
+            }
         }
-        
-        for(int i=0;i<b.length;i++)
-        {
-            set.add(b[i]);
+
+        while (i < a.length) {
+            if (!set.contains(a[i])) {
+                ans.add(a[i]);
+                set.add(a[i]); // Record element to block subsequent duplicates
+            }
+            i++;
         }
-        
-        ArrayList<Integer>ans=new ArrayList<>();
-        for(int x:set)
-        {
-            ans.add(x);
+
+        while (j < b.length) {
+            if (!set.contains(b[j])) {
+                ans.add(b[j]);
+                set.add(b[j]); // Record element to block subsequent duplicates
+            }
+            j++;
         }
-        
-       Collections.sort(ans);
-        
-        
+
         return ans;
-        
     }
 }
